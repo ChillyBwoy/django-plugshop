3dd4da7d641f2feaa2a6f812a3333c06855ac447
Eugene Cheltsov <chill.icp@gmail.com>
2012-05-09 16:46:16 +0400

    form action tag

--- plugshop/templatetags/plugshop.py
+++ plugshop/templatetags/plugshop.py
@@ -1,22 +1,18 @@
 # encoding: utf-8
-import markdown
-
 from django import template
-from django.forms.widgets import Select
 from django.utils.translation import ugettext as _
-from plugshop import settings
 
 register = template.Library()
 
 @register.filter
-def currency(value):
+def plugshop_currency(value):
     if value is None: return ""
 
     v = str(value)[::-1]
     return " ".join([v[i:i+3][::-1] for i in xrange(0, len(v), 3) ][::-1])
 
 @register.simple_tag
-def option(opt_type, value, name=None):
+def plugshop_option(opt_type, value, name=None):
     if opt_type == 'str':
         return str(value)
     elif opt_type == 'int':
@@ -30,3 +26,12 @@ def option(opt_type, value, name=None):
             })
         except ValueError:
             return ""
+
+
+@register.inclusion_tag('plugshop/tags/action.html')
+def plugshop_action(product, action, quantity=1):
+    return {
+        'product': product,
+        'action': action,
+        'quantity': quantity,
+    }
